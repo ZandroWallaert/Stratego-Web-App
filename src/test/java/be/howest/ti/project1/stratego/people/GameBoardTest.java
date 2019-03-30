2a22a41d76e13bae18d6d87ddce50975a602583c
Wallaert Zandro <zandro.wallaert@student.howest.be>
2019-03-30 16:45:18 +0100

    Gameboard Grid Update
    Signed-off-by: Wallaert Zandro <zandro.wallaert@student.howest.be>

--- src/test/java/be/howest/ti/project1/stratego/people/GameBoardTest.java
+++ src/test/java/be/howest/ti/project1/stratego/people/GameBoardTest.java
@@ -2,13 +2,10 @@ package be.howest.ti.project1.stratego.people;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class GameBoardTest {
 
     @Test
     public void getBoard() {
-
     }
 
     @Test
