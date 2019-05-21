eb17c8a176db32cedc7f8c314fa8cd327a919f10
Gabriel <gabriel.rousseau@student.howest.be>
2019-05-21 09:41:10 +0200

    fix onclicks removal merge
    Signed-off-by: Gabriel <gabriel.rousseau@student.howest.be>

--- src/main/resources/webroot/assets/js/eventListeners.js
+++ src/main/resources/webroot/assets/js/eventListeners.js
@@ -1,3 +1,23 @@
 "use strict";
 
-//Will add all event listeners here!
+document.addEventListener('DOMContentLoaded', addEvents);
+
+function addEvents() {
+
+    document.getElementById("showRules").addEventListener('click', showRules);
+    document.getElementById("showSettings").addEventListener('click', showSettings);
+    document.getElementById("confirmExit").addEventListener('click', confirmExit);
+
+    document.getElementById("theme").addEventListener('click', changeTheme);
+    document.getElementById("sound").addEventListener('click', changeSfx);
+    document.getElementById("music").addEventListener('click', changeMusic);
+    document.getElementById("reset").addEventListener('click', resetSettings);
+
+    document.getElementById("saveChanges").addEventListener('click', saveChanges);
+
+    document.getElementById("cancel").addEventListener('click', cancelSearch);
+
+}
+
+
+
